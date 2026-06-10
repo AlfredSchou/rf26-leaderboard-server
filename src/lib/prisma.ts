3963b3312997c37ad9697a459a82da7f476b9fba
AlfredSchou <abjarneg@live.dk>
2026-06-10 15:55:49 +0200

    DB URL updates

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg({ connectionString: process.env.HIGHSCORES_PRISMA_DATABASE_URL });
 const prisma: InstanceType<typeof PrismaClient> = new PrismaClient({ adapter });
 export default prisma;
