dd58e2bcfa876697a85efa1306335ba47910f2bb
AlfredSchou <abjarneg@live.dk>
2026-06-11 10:40:05 +0200

    dynamic loading

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -3,6 +3,7 @@
 import { useSearchParams } from "next/navigation";
 import { useState, Suspense } from "react";
 
+export const dynamic = "force-dynamic";
 
 export function SubmitPage() {
   const params = useSearchParams();
