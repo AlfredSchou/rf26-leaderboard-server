f14d76c0d68dfd0455022de6ba44748a6ace6738
AlfredSchou <abjarneg@live.dk>
2026-06-11 13:03:32 +0200

    more width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,13 +13,13 @@ export default async function Page() {
     <h1>HIGH SCORES</h1>
       <div className="flex justify-between gap-8 w-full max-w-md">
         <span className="w-12">RANK</span>
-        <span className="w-40">TAG</span>
+        <span className="w-50">TAG</span>
         <span className="w-20">SCORE</span>
       </div>    
     {scores.map((score, index) => (
       <div key={score.id} className="flex justify-between gap-8 w-full max-w-md">
         <span className="w-12">{index + 1}</span>
-        <span className="w-40">{score.tag.slice(0, 12)}</span>
+        <span className="w-50">{score.tag.slice(0, 12)}</span>
         <span className="w-20">{score.score}</span>
       </div>
     ))}
