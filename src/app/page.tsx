02e2b6eee6095afd0145e0ffcb887a6812d37f02
AlfredSchou <abjarneg@live.dk>
2026-06-11 12:09:01 +0200

    formatting landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,5 +9,27 @@ export default async function Page() {
     orderBy: { score: "desc" },
   });
 
-  return <div className={pixelFont.className}>{JSON.stringify(scores)}</div>;
+  return <main className={`${pixelFont.className} flex flex-col items-center justify-center min-h-screen gap-8`}>
+    <h1>HIGH SCORES</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>RANK</th>
+          <th>TAG</th>
+          <th>SCORE</th>
+          <th>DATE</th>
+        </tr>
+      </thead>
+      <tbody>
+        {scores.map((score, index) => (
+          <tr key={score.id}>
+            <td>{index + 1}</td>
+            <td>{score.tag}</td>
+            <td>{score.score}</td>
+            <td>{score.createdAt.toLocaleDateString()} {score.createdAt.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </main>
 }
