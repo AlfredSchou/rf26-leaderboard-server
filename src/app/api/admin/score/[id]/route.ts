395b78c248289527cd1c33e40c55b1696a4f2628
AlfredSchou <abjarneg@live.dk>
2026-06-15 13:33:10 +0200

    added promise

--- src/app/api/admin/score/[id]/route.ts
+++ src/app/api/admin/score/[id]/route.ts
@@ -2,7 +2,7 @@ import {cookies} from "next/headers";
 import {NextResponse} from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function DELETE(req: Request, {params}: {params: {id: string}}) {
+export async function DELETE(req: Request, {params}: {params: Promise<{id: string}>}) {
     const {id: rawId} = await params;
     const id = Number(rawId);
     const cookieStore = await cookies();
